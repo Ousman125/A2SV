ee2d4b573e41fb491400f5d6054afe979ff58526
Ousman125 <120787110+Ousman125@users.noreply.github.com>
2022-12-23 21:21:19 +0300

    Create valid-parentheses.java

---
+++ valid-parentheses.java
@@ -0,0 +1,14 @@
+class Solution {
+    boolean j;
+    public boolean isValid(String s) {
+        Stack<Integer> p = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            int q = "(){}[]".indexOf(s.substring(i, i + 1));
+            if(q % 2 == 1) {
+                if(p.isEmpty() || p.pop() != q - 1) return false;
+            } else p.push(q);
+        }
+        return p.isEmpty();
+           
+    }
+}
