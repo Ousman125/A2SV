ce9724b9fa25ec670d905c79523eccb0cb6c74dc
Ousman125 <120787110+Ousman125@users.noreply.github.com>
2023-01-21 16:22:43 +0300

    Create design-linked-list.java

---
+++ design-linked-list.java
@@ -0,0 +1,37 @@
+class MyLinkedList {
+    LinkedList<Integer> list;
+    public MyLinkedList() {
+        list=new LinkedList<>();
+    }
+    
+    public int get(int index) {
+        if(index>=list.size()) return -1;
+        return list.get(index);
+    }
+    
+    public void addAtHead(int val) {
+        list.addFirst(val);
+    }
+    
+    public void addAtTail(int val) {
+     list.add(val);   
+    }
+    
+    public void addAtIndex(int index, int val) {
+       if(index<=list.size()) list.add(index,val);
+    }
+    
+    public void deleteAtIndex(int index) {
+        if(index<list.size())list.remove(index);
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
