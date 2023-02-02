3fc96b0481d48b39968c2bf3808806a2c47a91e0
Ousman125 <120787110+Ousman125@users.noreply.github.com>
2023-02-02 16:58:48 +0300

    Create Domino piling.java

---
+++ Domino piling.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+    Scanner input=new Scanner(System.in);
+    int M=input.nextInt();
+    int N=input.nextInt();
+    int sol=M*N/2;
+    System.out.println(sol);
+    }
+    }
+  
